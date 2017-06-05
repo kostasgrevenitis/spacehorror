f2e7d39985927bf969e853bd5070e86e341218ee
Konstantinos Grevenitis <constantinos.grevenites@gmail.com>
2017-06-05 17:40:21 +0300

    updated success world

--- SuccessWorld.java
+++ SuccessWorld.java
@@ -8,7 +8,19 @@ public class SuccessWorld extends World {
         if(rand == 0) {
             rand = 10;
         }
-        
+
+        char[] randCharArray = Integer.toString(score).toCharArray();
+        int xPosition = 320-((100*(randCharArray.length-1))/randCharArray.length);
+
+        for(int i=0; i<randCharArray.length; i++){
+            GreenfootImage image = new GreenfootImage(randCharArray[i] + "_digit.png");
+            image.scale(100, 100);
+            Container c = new Container();
+            c.setImage(image);
+            addObject(c, xPosition, 250);
+            xPosition = xPosition + 110;
+         }
+
         ButtonGoToLevel goToLevel = new ButtonGoToLevel(rand);
         addObject(goToLevel, 320, 400);
     }
